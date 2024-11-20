8d766fac87d3707c199b5f2c15a8101a320daca0
Yadong Liu <liu0122@outlook.com>
2024-11-20 02:22:00 +0000

    modified CMake and nlopt_dependencies so it uses the proper hook

--- subpackages/mav_trajectory_generation/cmake_dependencies/nlopt_dependencies.cmake
+++ subpackages/mav_trajectory_generation/cmake_dependencies/nlopt_dependencies.cmake
@@ -4,12 +4,12 @@ if (${NLopt_FOUND})
   MESSAGE(STATUS "Found NLopt.")
 else (${NLopt_FOUND})
   MESSAGE(STATUS "Could not locate NLopt.")
-  include(FetchContent)
-  FetchContent_Declare(
-    NLopt
-    URL https://github.com/stevengj/NLopt/archive/09b3c2a6da71cabcb98d2c8facc6b83d2321ed71.zip
-  )
-  # For Windows: Prevent overriding the parent project's compiler/linker settings
-  set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-  FetchContent_MakeAvailable(NLopt)
+  # include(FetchContent)
+  # FetchContent_Declare(
+  #   NLopt
+  #   URL https://github.com/stevengj/NLopt/archive/09b3c2a6da71cabcb98d2c8facc6b83d2321ed71.zip
+  # )
+  # # For Windows: Prevent overriding the parent project's compiler/linker settings
+  # set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+  # FetchContent_MakeAvailable(NLopt)
 endif(${NLopt_FOUND})
